928010cc87d5762dfac623b50555940528bd0357
wisdom obioma <wisdomobiomadev@gmail.com>
2022-04-30 14:53:45 +0100

    create about section

---
+++ src/components/AboutCarousel/AboutCarousel.jsx
@@ -0,0 +1,35 @@
+import Carousel from "react-grid-carousel";
+
+import { StyledWrapper } from "./styles";
+
+import image1 from "../../assets/characters/about_carousel/1.png";
+import image2 from "../../assets/characters/about_carousel/2.png";
+import image3 from "../../assets/characters/about_carousel/3.png";
+import image4 from "../../assets/characters/about_carousel/4.png";
+
+const carouselImageArray = [image1, image2, image3, image4];
+
+const AboutCarousel = () => {
+  return (
+    <StyledWrapper>
+      <Carousel
+        cols={2}
+        rows={2}
+        gap={20}
+        loop
+        showDots={true}
+        hideArrow={true}
+      >
+        {carouselImageArray.map((image, i) => (
+          <Carousel.Item key={i}>
+            <div className="img-box">
+              <img src={image} alt="" />
+            </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </StyledWrapper>
+  );
+};
+
+export default AboutCarousel;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import About from "../../components/About/About";
 import { StyledWrapper, StyledContent } from "./styles";
 
 import rays from "../../assets/images/lightrays.png";
@@ -8,28 +9,31 @@ import characterThree from "../../assets/characters/3.png";
 
 const Home = () => {
   return (
-    <StyledWrapper>
-      <img src={rays} alt="" className="light-rays" />
-      <StyledContent>
-        <h1 className="hero-text">axgen</h1>
-        <p>
-          The lengendary creature that regenerates. Living eternally on the
-          Etheruem network
-        </p>
-        <div className="mint-btn">
-          <Link to="#">mint now</Link>
+    <>
+      <StyledWrapper>
+        <img src={rays} alt="" className="light-rays" />
+        <StyledContent>
+          <h1 className="hero-text">axgen</h1>
+          <p>
+            The lengendary creature that regenerates. Living eternally on the
+            Etheruem network
+          </p>
+          <div className="mint-btn">
+            <Link to="#">mint now</Link>
+          </div>
+        </StyledContent>
+        <div className="character character-one">
+          <img src={characterOne} alt="" />
         </div>
-      </StyledContent>
-      <div className="character character-one">
-        <img src={characterOne} alt="" />
-      </div>
-      <div className="character character-two">
-        <img src={characterTwo} alt="" />
-      </div>
-      <div className="character character-three">
-        <img src={characterThree} alt="" />
-      </div>
-    </StyledWrapper>
+        <div className="character character-two">
+          <img src={characterTwo} alt="" />
+        </div>
+        <div className="character character-three">
+          <img src={characterThree} alt="" />
+        </div>
+      </StyledWrapper>
+      <About />
+    </>
   );
 };
 
